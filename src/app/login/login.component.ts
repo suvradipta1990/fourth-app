147f43bc6f5ad283148b23fa2c8cbbbd575fd9d7
suvradipta1990 <suvradipta1990@gmail.com>
2020-08-08 12:38:40 +0530

    login and register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import {ngForm} from "@angular/forms";
 export * from './login.component';
 
 @Component({
